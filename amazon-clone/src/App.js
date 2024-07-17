ddd9c59edf288d3d348109b4c0ce7cc6d8dbcbb6
Rebecca Jeon <she4115@gmail.com>
2024-07-17 01:40:55 -0700

    Set Up & Start of Header

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+      <h1>Hello Clever Programmers, let's build the Amazon Store! </h1>
+      <Header/>
+      {/* Home */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDEx1azk6l7jsAmLlpN4Z2FnbVqHpX6Gq4",
+    authDomain: "clone-f6caa.firebaseapp.com",
+    projectId: "clone-f6caa",
+    storageBucket: "clone-f6caa.appspot.com",
+    messagingSenderId: "817590809378",
+    appId: "1:817590809378:web:8826b00d97287b30606b7b",
+    measurementId: "G-7Q0L54XYT2"
+  };

8fb25fe453a22321f9fc308e746da61b3c7a7aa9
Rebecca Jeon <she4115@gmail.com>
2024-07-22 16:38:11 -0700

    Started on Home.js and Product.js

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     // BEM
     <div className="app">
-      <h1>Hello Clever Programmers, let's build the Amazon Store! </h1>
-      <Header/>
-      {/* Home */}
-
+      <Header />
+      <Home />
     </div>
   );
 }

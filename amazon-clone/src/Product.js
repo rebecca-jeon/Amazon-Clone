4246b6f6b693e1aa51d392357310c6409e05648b
Rebecca Jeon <she4115@gmail.com>
2024-07-25 13:36:07 -0700

    used react-router-dom

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Product.css";
 
-function Product({ title, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
@@ -18,7 +18,7 @@ function Product({ title, image, price, rating }) {
             ))}
         </div>
       </div>
-      <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" />
+      <img src={image} />
 
       <button>Add to Basket</button>
     </div>

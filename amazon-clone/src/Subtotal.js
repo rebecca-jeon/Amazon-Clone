2c292ac06f7b4442bd4fc36765f3a2da7b1f41b1
Rebecca Jeon <rebeccajeon@Rebeccas-MacBook-Air-2.local>
2024-10-30 18:41:54 -0700

    Used Reducer

--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -1,8 +1,35 @@
 import React from "react";
 import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import {useStateValue} from "./StateProvider";
+import { getBasketTotal } from "./reducer";
 
 function Subtotal() {
-  return <div className="subtotal"></div>;
+  const [{basket}, dispatch] = useStateValue();
+
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        value={getBasketTotal(basket)}
+        displayType={'text'}
+        thousandSeparator={true}
+        decimalScale={2}
+        prefix={'$'}
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal ({basket?.length} items): <strong>{value}</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+      />
+
+      <button>Proceed to Checkout</button>
+    </div>
+  );
 }
 
 export default Subtotal;

343413c07c9763cbc639cfa0e44fe1d78e17314d
Rebecca Jeon <rebeccajeon@Rebeccas-MacBook-Air-2.local>
2024-12-04 22:07:31 -0800

    ADD: Payment.js,  Payment.css, Stripe

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -3,10 +3,17 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import Payment from './Payment';
 import Login from "./Login"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { auth } from './firebase';
 import { useStateValue } from "./StateProvider";
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+// completely safe to be in the open. It is a public key.
+const promise = loadStripe
+('pk_test_51QSXm8KNSQ0tsJ7cpaf1AEt7IEYfr6SzV5OfDKELTuSLrFJgMItseGPIOvX9GKed0Z3TfxzA8mME1pOKQhJiHJiD00LxWJcm5b');
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -50,6 +57,17 @@ function App() {
               <>
                 <Header />
                 <Checkout />
+              </>
+            }
+          />
+          <Route
+            path="/payment"
+            element={
+              <>
+                <Header />
+                <Elements stripe = {promise}>
+                  <Payment />
+                </Elements>
               </>
             }
           />
--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -3,8 +3,15 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import {useStateValue} from "./StateProvider";
 import { getBasketTotal } from "./reducer";
+import { useNavigate } from  'react-router-dom';
 
+//version6 of react-router-dom removed 'useHistory' and replaced it with 'useNavigate'
+
+//navigate is good for when you need to push the user somewhere
+//allows to keep styling of the button while redirect.
+//If use Link, it will look like a link
 function Subtotal() {
+  const navigate = useNavigate();
   const [{basket}, dispatch] = useStateValue();
 
   return (
@@ -27,7 +34,7 @@ function Subtotal() {
         )}
       />
 
-      <button>Proceed to Checkout</button>
+      <button onClick={e => navigate('/payment')}>Proceed to Checkout</button>
     </div>
   );
 }

5aa9533d1b17cef55107b8b485c2b343bda06df6
Rebecca Jeon <rebeccajeon@Rebeccas-MacBook-Air-2.local>
2025-01-07 21:40:09 -0800

    axios, firebase API, Orders Page

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -5,6 +5,7 @@ import Home from "./Home";
 import Checkout from "./Checkout";
 import Payment from './Payment';
 import Login from "./Login"
+import Orders from "./Orders";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { auth } from './firebase';
 import { useStateValue } from "./StateProvider";
@@ -44,9 +45,18 @@ function App() {
       <div className="app">
         <Routes>
           <Route 
+            path = "/orders"
+            element = {
+              <>
+                <Orders />
+              </>
+            }
+          />
+          <Route 
             path = "/login"
             element = {
               <>
+                <Header />
                 <Login />
               </>
             }
